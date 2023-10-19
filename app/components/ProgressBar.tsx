6d47204fdb7a78c7b33ac268469191c6b382c061
yqow <yingqian.ow@hotmail.com>
2023-10-19 16:42:34 +0800

    Added in progress bars for the 3 data

---
+++ app/components/ProgressBar.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+interface ProgressBarProps {
+  value: number;
+  acceptableRange: { min: number; max: number };
+  barRange: { lower: number; upper: number };
+}
+
+const Progressbar: React.FC<ProgressBarProps> = ({ value, acceptableRange, barRange }) => {
+  let percentage = 0;
+  
+  percentage = ((value - barRange.lower)/(barRange.upper - barRange.lower)) * 100;
+  let progressColorClass = 'green'; // Default to green
+  if (value > acceptableRange.min && value < acceptableRange.max) {
+    progressColorClass = 'light-green'
+  }
+  else if (value > acceptableRange.min * 0.9 && value < acceptableRange.max * 1.1 ) {
+    progressColorClass = 'yellow'; // If outside +-10%, set to yellow
+  } else {
+    progressColorClass = 'red';
+  }
+
+  return (
+    <div className="progressbar-container">
+      <div
+        className={`progressbar ${progressColorClass}`}
+        style={{ width: `${percentage}%` }}
+      >
+        <style jsx>{`
+          .progressbar-container {
+            width: 100%;
+            height: 20px;
+            border: 1px solid #ccc;
+            position: relative;
+          }
+          .progressbar {
+            height: 100%;
+            transition: width 0.5s;
+          }
+          .light-green {
+            background-color: lightgreen;
+          }
+          .yellow {
+            background-color: yellow;
+          }
+          .red {
+            background-color: red;
+          }
+        `}</style>
+      </div>
+    </div>
+  );
+};
+
+export default Progressbar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useEffect, useState } from 'react';
+import ProgressBar from './components/ProgressBar';
 
 interface SensorData {
   co2: number;
@@ -42,12 +43,15 @@ export default function Home() {
 
   return (
     <div>
-      <h1>Sensor Data</h1>
+      <h1> <strong>Sensor Data</strong></h1>
       {sensorData ? (
         <div>
-          <p>CO2: {sensorData.co2} ppm</p>
-          <p>Temperature: {sensorData.temperature} °C</p>
-          <p>Humidity: {sensorData.humidity} RH</p>
+          <p> <strong>CO2:</strong> {sensorData.co2} ppm</p>
+          <ProgressBar value={sensorData.co2} acceptableRange={{ min: 800, max: 1500 }} barRange={{lower: 0, upper: 3000 }} />
+          <p> <strong>Temperature:</strong> {sensorData.temperature} °C</p>
+          <ProgressBar value={sensorData.temperature} acceptableRange={{ min: 21.1, max: 26.6 }} barRange={{ lower: 16, upper: 50 }} />   
+          <p> <strong>Humidity:</strong> {sensorData.humidity} RH</p>
+          <ProgressBar value={sensorData.humidity} acceptableRange={{ min: 70, max: 80 }} barRange={{lower: 0, upper: 100 }} />
         </div>
       ) : (
         <p>Loading data...</p>
